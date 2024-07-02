43bd8d14abbfa022fd21292aaa07d1f47a1ab46d
Lucas Duarte <73208615+lucasdrte@users.noreply.github.com>
2024-07-02 20:24:29 -0300

    Testes de integração - cobertura 100%

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -12,7 +12,7 @@ test('Testando banco de dados vazio', () => {
   expect(modelo.listar_perguntas().length).toBe(0);
 });
 
-test('Testando cadastro de três perguntas', () => {
+test('Testando cadastro de três perguntas e respostas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
@@ -20,6 +20,18 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
+  expect(perguntas[2].texto).toBe('3 + 3 = ?');
+  expect(perguntas[0].num_respostas).toBe(0);
+  expect(perguntas[1].num_respostas).toBe(0);
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  modelo.get_pergunta(0);
+  modelo.get_pergunta(1);
+  modelo.get_pergunta(2);
+  modelo.cadastrar_resposta(0, '2');
+  modelo.cadastrar_resposta(1, '4');
+  modelo.cadastrar_resposta(2, '6');
+  modelo.get_respostas(0);
+  modelo.get_respostas(1);
+  modelo.get_respostas(2);
 });
